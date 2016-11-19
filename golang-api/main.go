e49ecc053e1209afb4983f3d31c555a21e10fcae
gsingharoy <neogauravsvnit@gmail.com>
2016-11-19 15:28:42 +0100

    Created basic actions say "minions"
    Created basic action called minions in the golang-api repo.

--- golang-api/main.go
+++ golang-api/main.go
@@ -5,13 +5,31 @@ import (
     "html"
     "log"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    router.HandleFunc("/minions", MinionIndex)
+    router.HandleFunc("/minions/{minionId}", MinionShow)
 
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+
+func MinionIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "The minion Index page!")
+}
+
+func MinionShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    minionId := vars["minionId"]
+    fmt.Fprintln(w, "Minion show:", minionId)
 }
